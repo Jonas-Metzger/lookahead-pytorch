c1c28401014c587021bc9c440c2ee42bc68a0309
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2022-09-07 15:43:43 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="lookahead",
+    version="0.1",
+    author="Jonas Metzger",
+    author_email="",
+    description="Lookahead Optimizer Wrapper",
+    url="https://github.com/Jonas-Metzger/lookahead-pytorch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
