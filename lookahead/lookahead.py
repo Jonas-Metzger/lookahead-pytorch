4a151e18f4bd7859a34d3181dac7433106642d9b
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2023-08-18 16:42:32 -0700

    Update lookahead.py

--- lookahead/lookahead.py
+++ lookahead/lookahead.py
@@ -147,14 +147,14 @@ class OAdam(torch.optim.Optimizer):
                 beta1, beta2 = group['betas']
                 state['step'] += 1
                 if group['weight_decay'] != 0:
-                    grad.add_(group['weight_decay'], p.data)
+                    grad.add_(p.data, alpha=group['weight_decay'])
                 bias_correction1 = 1 - beta1 ** state['step']
                 bias_correction2 = 1 - beta2 ** state['step']
                 step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
                 # Optimistic update :)
                 p.data.addcdiv_(exp_avg, exp_avg_sq.sqrt().add(group['eps']), value=step_size)
                 # Decay the first and second moment running average coefficient
-                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
                 exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
                 if amsgrad:
                     # Maintains the maximum of all 2nd moment running avg. till now
